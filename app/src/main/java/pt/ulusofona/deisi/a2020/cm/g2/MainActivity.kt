d3706cc48b01fb94be29a1fa2e616b8171583595
João Cardoso <44017763+joaocardosoit@users.noreply.github.com>
2021-03-29 20:33:01 +0100

    Primeiro commit

---
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g2/MainActivity.kt
@@ -0,0 +1,11 @@
+package pt.ulusofona.deisi.a2020.cm.g2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
