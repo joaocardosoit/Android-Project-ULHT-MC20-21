2aa2bac3a6e8055b5b1f4e30302ffa56a695fabe
Dinis-21600880 <56201669+Dinis-21600880@users.noreply.github.com>
2021-04-02 19:33:26 +0100

    Criação do ContactFragment. Screen dos contactos feito.

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g2/fragments/ContactosFragment.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g2/fragments/ContactosFragment.kt
@@ -27,5 +27,27 @@ class ContactosFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        mostrarContactos()
     }
+
+    fun mostrarContactos(){
+        val contactosTelefonico: MutableList<String> = ArrayList()
+        contactosTelefonico.add(getString(R.string.num_telefone_1) + "\n" + getString(R.string.descricao_contactos_1))
+        contactosTelefonico.add(getString(R.string.num_telefone_2) + "\n" + getString(R.string.descricao_contactos_2))
+        contactosTelefonico.add(getString(R.string.num_telefone_3) + " e " + getString(R.string.num_telefone_4) + "\n" + getString(R.string.descricao_contactos_3))
+
+        val contactosDigitias: MutableList<String> = ArrayList()
+        contactosDigitias.add(getString(R.string.contacto_digital_1) + "\n" + getString(R.string.descricao_contacto_digital_1))
+        contactosDigitias.add(getString(R.string.contacto_digital_2) + "\n" + getString(R.string.descricao_contacto_digital_2))
+        contactosDigitias.add(getString(R.string.contacto_digital_3) + "\n" + getString(R.string.descricao_contacto_digital_3))
+        contactosDigitias.add(getString(R.string.contacto_digital_4) + "\n" + getString(R.string.descricao_contacto_digital_4))
+
+
+        assunto.add(getString(R.string.contactos_telefonicos))
+        assunto.add(getString(R.string.contactos_digitais))
+        conteudo.add(contactosTelefonico)
+        conteudo.add(contactosDigitias)
+
+    }
+
 }
