e375705a100ee15c54e36d49eb365b3618038029
João Cardoso <44017763+joaocardosoit@users.noreply.github.com>
2021-04-07 04:30:13 +0100

    Fragmento detalhes funcional

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g2/fragments/DetalhesTesteFragment.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g2/fragments/DetalhesTesteFragment.kt
@@ -6,17 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_detalhes_teste.*
 import pt.ulusofona.deisi.a2020.cm.g2.R
+import pt.ulusofona.deisi.a2020.cm.g2.activities.testes
+import pt.ulusofona.deisi.a2020.cm.g2.models.Teste
 import pt.ulusofona.deisi.a2020.cm.g2.utils.NavigationManager
 
 class DetalhesTesteFragment : Fragment() {
+
+    var teste: Teste? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_destalhes_teste, container, false)
+        teste = arguments?.getParcelable("Teste") as Teste
+        return inflater.inflate(R.layout.fragment_detalhes_teste, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -27,6 +33,20 @@ class DetalhesTesteFragment : Fragment() {
             activity?.toolbar_main?.title = "MyCovid-19"
             activity?.toolbar_main?.navigationIcon = null
             activity?.supportFragmentManager?.let { NavigationManager.goToListaFragment(it) }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        detalhesTeste(teste)
+    }
+
+    private fun detalhesTeste(teste: Teste?){
+        teste?.let {
+            image_descricao.setImageResource(teste.imagem!!)
+            texta_local_descricao.text = getString(R.string.teste_realizado_em) + teste.local
+            texta_data_descricao.text = getString(R.string.teste_feito_a) + teste.data
+            texta_resultado_descricao.text = getString(R.string.o_resultado_do_teste_foi) + teste.resultado
         }
     }
 }
