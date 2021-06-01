bd2a3e47326871a9b16a12a8b2d65e1b24bc8747
Dinis <diniscabraloliveira@hotmail.com>
2021-06-01 19:54:22 +0100

    Correção de Bug relativo à conectividade.

--- app/src/main/java/pt/ulusofona/ecati/deisi/licenciatura/cm2021/grupo2/ui/utils/Connectivity.kt
+++ app/src/main/java/pt/ulusofona/ecati/deisi/licenciatura/cm2021/grupo2/ui/utils/Connectivity.kt
@@ -9,7 +9,7 @@ abstract class Connectivity private constructor(){
     companion object{
         fun isConnected(context: Context): Boolean{
             val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            val activeNetwork: NetworkInfo = connectivityManager.activeNetworkInfo
+            val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
             return if(activeNetwork?.isConnected!=null){
                 activeNetwork.isConnected
             } else{
